2598591c084e6b4ac5ea10958f1446e9d0ef8c01
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2021-01-08 00:06:21 +0300

    cw7

--- question1.js
+++ question1.js
@@ -1,1 +1,28 @@
 // قم بحل السؤال الأول هنا
+let Pets = [
+    "CATS",
+    "Dogs",
+    "Turtles",
+    "Hamsters",
+    "Birds",
+]
+console.log(Pets)
+Pets.pop()
+
+let newPet = [
+    "Flamingo"
+]
+Pets.push(newPet);
+Pets[2]="monkey";
+
+let emoji = `🐱`
+
+let cat = {
+    name:`Cat`,
+    age:`5`,
+    origin:`sphynx`,
+    emoji: emoji
+}
+console.log(cat.age)
+
+console.log(cat.emoji)
